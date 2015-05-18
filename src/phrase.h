98c05bb7bae7e39a4d5cbcc9af6e068307bb07ba
matjakov <matjakov@gmail.com>
2015-05-18 12:23:51 +0200

    -source reworked -Haiku tooltips, colors and date localization -navigation improvements -image loading bugs fixed -UI glitches fixed -redundant messaging removed -new preferences -"rebuild thumbnails" menu option -mod. time label added -experimental clipboard support

---
+++ src/phrase.h
@@ -0,0 +1,22 @@
+// optional or longer strings
+
+#define S_ABOUT_TEXT "Album (Code Name Aberration) v0.9.4\nCopyright "B_UTF8_COPYRIGHT" 2006-2015 by Matjaž Kovač.\n\nThis program is distributed under the terms of the MIT License (see LICENSE).\nIncludes libiptcdata by David Moore under the terms of the GNU LGPL.\n\nRecompiled on "__DATE__
+
+#define S_DROPFILES _("Drop files here.")
+#define S_STOP_SUB NULL
+#define S_STOP_TIP _("Stop loading/querying.")
+#define S_REMOVE_SUB NULL
+#define S_REMOVE_TIP _("Remove selected items from view.")
+#define S_COPYTAG_SUB NULL
+#define S_COPYTAG_TIP _("Copy selected tags to file attributes.")
+#define S_MARK_SUB NULL
+#define S_MARK_TIP _("Mark selected items.")
+#define S_PROGRESS_TIP _("Total item count")
+#define S_ZOOM_TIP _("Scale down.")
+#define S_TAGS_TIP ""
+#define S_ATTRS_TIP _("File attributes, drag tags here,  ENTER to edit.")
+#define S_RENAME_TIP _("Leave '*' in the filename to renumber multiple items.")
+#define S_THUMBNAIL_SIZE_TIP _("When writing thumbnail file attributes.")
+#define S_THUMBNAIL_NAME_TIP _("These file attributes are always checked first for image previews.")
+#define S_FLICKER_TIP _("Use extra memory for a steadier display when scrolling etc.")
+#define S_RELOAD_TIP _("Reload existing items.")
